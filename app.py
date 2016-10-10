159da4c152abce11bd15891c7596d48429376ebc
Maxim Pavlov <maxpav137@gmail.com>
2016-10-10 21:02:51 +0500

    Improving logging system

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import asyncio
 import logging
 import pathlib
+from logging.config import dictConfig
+
 from aiohttp import web
+from aiohttp.log import server_logger
+# from logger import setup_logger
+
 
 from middlewares import setup_middlewares
 from routes import setup_routes
@@ -11,7 +16,7 @@ PROJ_ROOT = pathlib.Path(__file__).parent
 
 
 async def init(loop):
-    app = web.Application(loop=loop)
+    app = web.Application(loop=loop, debug=True)
 
     setup_routes(app, PROJ_ROOT)
     setup_middlewares(app)
@@ -21,8 +26,9 @@ async def init(loop):
 
 
 def main():
-    logging.basicConfig(level=conf['LOGGER_LEVEL'])
-
+    # logging.basicConfig(level=conf['LOGGER_LEVEL'])
+    # log = setup_logger()
+    dictConfig(conf['LOGGING_CONF'])
     loop = asyncio.get_event_loop()
     app, host, port = loop.run_until_complete(init(loop))
     web.run_app(app, host=host, port=port)
--- config.py
+++ config.py
@@ -1,5 +1,34 @@
+import logging
+
+LOGGING_LEVEL=logging.DEBUG
+
 conf = dict(
-    LOGGER_LEVEL='DEBUG',
     HOST='0.0.0.0',
-    PORT='31337'
+    PORT='31337',
+
+    LOGGING_CONF=dict(
+            version=1,
+            disable_existing_loggers=False,
+            formatters={
+                'f': {'format':
+                          '%(levelname)s:%(name)s: %(message)s (%(asctime)s; %(filename)s:%(lineno)d'}
+            },
+            handlers={
+                'console': {'class': 'logging.StreamHandler',
+                            'formatter': 'f',
+                            'level': LOGGING_LEVEL},
+
+                'file': {'class': 'logging.handlers.RotatingFileHandler',
+                         'filename': 'root.log',
+                         'backupCount': 3,
+                         'maxBytes': 10000000,
+                         'encoding': 'utf8',
+                         'formatter': 'f',
+                         'level': LOGGING_LEVEL}
+            },
+            root={
+                'handlers': ['console', 'file'],
+                'level': LOGGING_LEVEL,
+            }
+    )
 )
