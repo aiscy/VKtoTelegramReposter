d262ba0d05992e37425681f6c8798a00ff0355fd
Maxim Pavlov <maxpav137@gmail.com>
2016-10-11 13:44:37 +0500

    Just for fun :D

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -1,1 +1,5 @@
+import aiohttp.web
 from endpoints.lc import lc_callback
+
+async def root(request):
+    return aiohttp.web.HTTPFound('https://www.youtube.com/watch?v=V4MF2s6MLxY')
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 import aiohttp.web
 
-from endpoints import lc_callback
+from endpoints import lc_callback, root
 
 router = aiohttp.web.UrlDispatcher()
 router.add_post('/callback/lc', lc_callback)
+router.add_get('/', root)
